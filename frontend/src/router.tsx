f57e8566936796c4c63480c7362b257f699cc1cb
Dan Licht <dan.licht@moodys.com>
2023-11-13 20:58:26 -0500

    add base path

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -9,41 +9,44 @@ import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Readme from 'pages/Readme';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/readme',
-    element: <Readme />
-  },
-  {
-    path: '/env',
-    element: <Env />
-  },
-  {
-    path: '/conversation/:id?',
-    element: <Conversation />
-  },
-  {
-    path: '/element/:id',
-    element: <Element />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/design',
-    element: <Design />
-  },
-  {
-    path: '/login/callback',
-    element: <AuthCallback />
-  },
-  {
-    path: '*',
-    element: <Navigate replace to="/" />
-  }
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/readme',
+      element: <Readme />
+    },
+    {
+      path: '/env',
+      element: <Env />
+    },
+    {
+      path: '/conversation/:id?',
+      element: <Conversation />
+    },
+    {
+      path: '/element/:id',
+      element: <Element />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    },
+    {
+      path: '/design',
+      element: <Design />
+    },
+    {
+      path: '/login/callback',
+      element: <AuthCallback />
+    },
+    {
+      path: '*',
+      element: <Navigate replace to="/" />
+    }
+  ],
+  { basename: '/investigations/assistant' }
+);
